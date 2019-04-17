27fbda9a7888593861d5ca12b129e33a7739ca9e
Latwan Wesley <wesleylatwan22@gmail.com>
2019-04-17 16:02:42 -0600

    MVP

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
+    
 export const INCREMENT = 'INCREMENT';
 export const DECREMENT = 'DECREMENT';
 
@@ -11,8 +12,30 @@ export const DECREMENT = 'DECREMENT';
 
 export const increment = () => {
   // Fill in this function
+  return { type: INCREMENT }
 };
 
 export const decrement = () => {
   // Fill in this function
+  return { type: DECREMENT }
+};    
+export const INCREMENT = 'INCREMENT';
+export const DECREMENT = 'DECREMENT';
+
+// Our action creators will return
+// an action packet that our reducer will
+// receive. What does the action packet look like?
+// Note that the action creator is not at all
+// responsible for handling any of the actual
+// logic of updating the central Redux store. That
+// is left to the reducer(s).
+
+export const increment = () => {
+  // Fill in this function
+  return { type: INCREMENT }
+};
+
+export const decrement = () => {
+  // Fill in this function
+  return { type: DECREMENT }
 };
